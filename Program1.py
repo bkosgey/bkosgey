fdadaeb3b55824c9319921fccb0448ff0195726e
bkosgey <52126967+bkosgey@users.noreply.github.com>
2022-08-27 17:08:08 +0300

    Update Program1.py

--- Program1.py
+++ Program1.py
@@ -1,3 +1,9 @@
-inp = input('Europe floor')
-usf = int(input) + 1
-print('US floor',usf)
+class Child():
+  def __init__(self,name, age, grade):
+    self.name == name
+    self.age == age
+    self.grade == grade
+name = input('please enter your full names': )
+age = int(input('Please enter your age': )
+grade = input('Please enter your grade': )
+print('name')
