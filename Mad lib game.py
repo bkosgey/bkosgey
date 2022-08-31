614559485e0962032f2e55cd4800fb8fce17ebda
bkosgey <52126967+bkosgey@users.noreply.github.com>
2022-08-31 06:37:40 +0300

    Add files via upload

---
+++ Mad lib game.py
@@ -0,0 +1,43 @@
+
+
+noun = input('choose a name: ')
+
+place = input('name of a place: ')
+
+pronoun = input('choose a pronoun: ')
+
+verb = input('enter a verb: ')
+
+adjective = input('choose an adjective: ')
+
+pronouns = input('choose pronoun: ')
+
+nouns = input('name of your favourite celebrity: ')
+
+verbs = input('choose a verb: ')
+
+
+print('A government clerk,' + noun,)
+
+print('sneezes while at the,' + place,)
+
+print('Accidentally spraying the man in front of ' + pronoun,'.')
+
+print('The man ' + verb, ' in another government department''.')
+
+print('Ivan is terribly ' + adjective,'.')
+
+print(pronouns + ' apologizes and is told to forget it''.')
+
+print(nouns + ' can’t relax after his faux pas ''.')
+
+print('He ' + verbs + ' to be sure everything is fine''.')
+
+
+
+
+
+
+
+
+
